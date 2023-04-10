a6447844ed26eef4c00a417288bbc5d784783a9b
07shrutik <shrutikmahajan1414@gmail.com>
2023-04-10 16:21:19 +0530

    React Revision Assignment 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Assign1 from "./components/Assign1";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <Assign1 />
+    </div>
+  );
+};
+
+export default App;
