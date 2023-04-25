33c97f185107f51878bd08c3d4369df30e7e6227
07shrutik <shrutikmahajan1414@gmail.com>
2023-04-25 22:02:03 +0530

    Redux assignment 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 // import "./style.css";
-import Todo from "./Todo";
+// import Todo from "./Todo";
 import { Provider } from "react-redux";
 import { store } from "./Slic";
 import Count from "./Count";
@@ -9,9 +9,6 @@ export default function App() {
   return (
     <Provider store={store}>
       <div>
-        {/* <h1>Redux :- 1</h1> */}
-        <Todo />
-        <h1>Redux :- 1.1</h1>
         <Count />
       </div>
     </Provider>
