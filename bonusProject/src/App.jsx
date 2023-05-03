fd7c7ed81da6360309f50a9420e9e7165f15a1c9
07shrutik <shrutikmahajan1414@gmail.com>
2023-05-03 18:18:29 +0530

    Calculator assignment

--- bonusProject/src/App.jsx
+++ bonusProject/src/App.jsx
@@ -1,31 +1,60 @@
-// import * as React from "react";
 import { useState } from "react";
-import Select from "react-select";
+import "./App.css";
 
 export default function App() {
-  const [value, setValue] = useState(null);
-  const options = [
-    { value: "Ms Dhoni", label: "Ms Dhoni" },
-    { value: "Virat", label: "Virat" },
-    { value: "Jadeja", label: "Jadeja" },
-    { value: "Ruturaj", label: "Ruturaj" },
+  let numbers = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "+",
+    "-",
+    "*",
+    "/",
+    "%",
+    ".",
   ];
+  // let operators=[,]
+  const [value, setValue] = useState("");
+  const [count, setcount] = useState(0);
+  function handleClick(id) {
+    if (value === "0") {
+      setValue(numbers[id]);
+    } else {
+      if (count === 1) {
+        setcount(0);
+        setValue(numbers[id]);
+        return;
+      }
+      setValue(value + numbers[id]);
+    }
+  }
+  function handleres() {
+    const result = eval(value);
+    setValue(result);
+    setcount(count + 1);
+  }
+  function handleClear() {
+    setValue("");
+  }
   return (
-    <div style={{ margin: 20, width: 200 }}>
-      <Select
-        options={options}
-        defaultValue={value}
-        placeholder="Select your Player"
-        onChange={setValue}
-        isMulti
-        isSearchable
-        noOptionsMessage={() => "No Player Found"}
-        styles={{
-          clearIndicator: () => ({
-            color: "red",
-          }),
-        }}
-      />
+    <div className="container">
+      <input value={value} />
+      <div className="numbers">
+        {numbers.map((num, index) => (
+          <button key={index} onClick={(e) => handleClick(index)}>
+            {num}
+          </button>
+        ))}
+        <button onClick={handleres}>=</button>
+        <button onClick={handleClear}>clear</button>
+      </div>
     </div>
   );
 }
---
+++ bonusProject/src/assets/DropDown.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Select from "react-select";
+
+export default function App() {
+  const [value, setValue] = useState(null);
+  const options = [
+    { value: "Ms Dhoni", label: "Ms Dhoni" },
+    { value: "Virat", label: "Virat" },
+    { value: "Jadeja", label: "Jadeja" },
+    { value: "Ruturaj", label: "Ruturaj" },
+  ];
+  return (
+    <div style={{ margin: 20, width: 200 }}>
+      <Select
+        options={options}
+        defaultValue={value}
+        placeholder="Select your Player"
+        onChange={setValue}
+        isMulti
+        isSearchable
+        noOptionsMessage={() => "No Player Found"}
+        styles={{
+          clearIndicator: () => ({
+            color: "red",
+          }),
+        }}
+      />
+    </div>
+  );
+}
