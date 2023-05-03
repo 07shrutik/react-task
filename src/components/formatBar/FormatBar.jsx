bf31630b193db583d520bd837ae848b56828d2eb
robin <rcgaur84@gmail.com>
2023-05-03 20:28:56 +0530

    Formatting Bar options

---
+++ src/components/formatBar/FormatBar.jsx
@@ -0,0 +1,32 @@
+import {formatBar} from "../../constants/commonData"
+import styles from './FormatBar.module.css'
+
+
+
+
+export default function FormatBar() {
+  return (
+    <div>
+      <ul className={styles.formatBarIcons}>
+        {formatBar.map(icon=><li style={{margin: "0.2rem", color:"rgb(93, 91, 91)"}}key={icon.description}>{icon.icons}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+
+// import RedoIcon from "@mui/icons-material/Redo";
+// import FormatBoldIcon from "@mui/icons-material/FormatBold";
+// import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+// import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+// import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
+// import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
+// import FormatAlignRightIcon from "@mui/icons-material/FormatAlignRight";
+// import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+// import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+// import style from "../Navbar/Navbar.module.css"
+// import SubscriptIcon from "@mui/icons-material/Subscript";
+// import SuperscriptIcon from "@mui/icons-material/Superscript";
+// import FormatIndentDecreaseIcon from "@mui/icons-material/FormatIndentDecrease";
+// import FormatIndentIncreaseIcon from "@mui/icons-material/FormatIndentIncrease";
+// import StrikethroughSIcon from '@mui/icons-material/StrikethroughS';
---
+++ src/constants/commonData.jsx
@@ -0,0 +1,122 @@
+import UndoIcon from '@mui/icons-material/Undo';
+import RedoIcon from '@mui/icons-material/Redo';
+import PrintOutlinedIcon from '@mui/icons-material/PrintOutlined';
+import SpellcheckOutlinedIcon from '@mui/icons-material/SpellcheckOutlined';
+import ImagesearchRollerOutlinedIcon from '@mui/icons-material/ImagesearchRollerOutlined';
+import RemoveOutlinedIcon from '@mui/icons-material/RemoveOutlined';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import FormatBoldOutlinedIcon from '@mui/icons-material/FormatBoldOutlined';
+import FormatItalicOutlinedIcon from '@mui/icons-material/FormatItalicOutlined';
+import FormatUnderlinedOutlinedIcon from '@mui/icons-material/FormatUnderlinedOutlined';
+import FormatColorTextOutlinedIcon from '@mui/icons-material/FormatColorTextOutlined';
+import ModeOutlinedIcon from '@mui/icons-material/ModeOutlined';
+import InsertLinkOutlinedIcon from '@mui/icons-material/InsertLinkOutlined';
+import AddCommentOutlinedIcon from '@mui/icons-material/AddCommentOutlined';
+import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
+import FormatAlignLeftOutlinedIcon from '@mui/icons-material/FormatAlignLeftOutlined';
+import FormatLineSpacingOutlinedIcon from '@mui/icons-material/FormatLineSpacingOutlined';
+import ChecklistOutlinedIcon from '@mui/icons-material/ChecklistOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
+import FormatListNumberedOutlinedIcon from '@mui/icons-material/FormatListNumberedOutlined';
+import FormatIndentDecreaseOutlinedIcon from '@mui/icons-material/FormatIndentDecreaseOutlined';
+import FormatIndentIncreaseOutlinedIcon from '@mui/icons-material/FormatIndentIncreaseOutlined';
+import FormatClearOutlinedIcon from '@mui/icons-material/FormatClearOutlined';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+
+
+const formatBar=[
+    {
+        icons: <UndoIcon />,
+        description: "Undo (Ctrl+Z)"
+    },
+    {
+        icons: <RedoIcon />,
+        description: "Redo (Ctrl+Y)"
+    },
+    {
+        icons: <PrintOutlinedIcon />,
+        description: "Print"
+    },
+    {
+        icons: <SpellcheckOutlinedIcon />,
+        description: "Spell and Grammar check"
+    },
+    {
+        icons: <ImagesearchRollerOutlinedIcon />,
+        description: "Paint format"
+    },
+    {
+        icons: <RemoveOutlinedIcon />,
+        description: "Decrease font size (Ctrl+Z)"
+    },
+    {
+        icons: <AddOutlinedIcon />,
+        description: "Increase font size (Ctrl+Z)"
+    },
+    {
+        icons: <FormatBoldOutlinedIcon />,
+        description: "Bold (Ctrl+Z)"
+    },
+    {
+        icons: <FormatItalicOutlinedIcon />,
+        description: "Italic (Ctrl+Z)"
+    },
+    {
+        icons: <FormatUnderlinedOutlinedIcon />,
+        description: "Underline (Ctrl+Z)"
+    },
+    {
+        icons: <FormatColorTextOutlinedIcon />,
+        description: "Text color"
+    },
+    {
+        icons: <ModeOutlinedIcon />,
+        description: "Highlight color (Ctrl+Z)"
+    },
+    {
+        icons: <InsertLinkOutlinedIcon />,
+        description: "Insert link (Ctrl+K)"
+    },
+    {
+        icons: <AddCommentOutlinedIcon />,
+        description: "Add comment (Ctrl+Z)"
+    },
+    {
+        icons: <InsertPhotoOutlinedIcon />,
+        description: "Insert image(Ctrl+Z)"
+    },
+    {
+        icons: <FormatAlignLeftOutlinedIcon />,
+        description: "Align & indent (Ctrl+Z)"
+    },
+    {
+        icons: <FormatLineSpacingOutlinedIcon />,
+        description: "Line & paragraph spacing (Ctrl+Z)"
+    },
+    {
+        icons: <ChecklistOutlinedIcon />,
+        description: "Checklist"
+    },
+    {
+        icons: <FormatListBulletedOutlinedIcon />,
+        description: "Bulleted list (Ctrl+Z)"
+    },
+    {
+        icons: <FormatListNumberedOutlinedIcon />,
+        description: "Numbered list (Ctrl+Z)"
+    },
+    {
+        icons: <FormatIndentDecreaseOutlinedIcon />,
+        description: "Decrease indent (Ctrl+Z)"
+    },
+    {
+        icons: <FormatIndentIncreaseOutlinedIcon />,
+        description: "Increase indent"
+    },
+    {
+        icons: <FormatClearOutlinedIcon />,
+        description: "Clear formatting"
+    },
+]
+
+export {formatBar}
