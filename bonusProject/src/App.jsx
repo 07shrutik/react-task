b35e9aade05823ad2a0f81ec5647f4cba3c5f785
07shrutik <shrutikmahajan1414@gmail.com>
2023-05-03 09:28:10 +0530

    Searchable DropDown Menu

--- bonusProject/src/App.jsx
+++ bonusProject/src/App.jsx
@@ -1,102 +1,31 @@
-import { useState, useEffect } from "react";
-// import './style.css';
+// import * as React from "react";
+import { useState } from "react";
+import Select from "react-select";
 
 export default function App() {
-  const [data, setdata] = useState([]);
-  const [filtervalue, setfiltervalue] = useState([]);
-  const [selectedvalue, setselectedvalue] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.npms.io/v2/search?q=reactjs")
-      .then((res) => res.json())
-      .then((data) => setdata(data.results));
-  }, []);
-
-  function handleChange(e) {
-    let res = e.target.value;
-    let filtervalues = data.filter((value) => {
-      return value.package.name.toLowerCase().includes(res.toLowerCase());
-    });
-
-    if (res === "") {
-      setfiltervalue([]);
-    } else {
-      setfiltervalue(filtervalues);
-      console.log(filtervalues);
-    }
-  }
-  let inputvalue;
-  function handleChangeinput(e) {
-    inputvalue = e.target.value;
-    console.log(inputvalue);
-  }
-  function handleClick(id) {
-    let result = filtervalue.filter((value, index) => index === id);
-    let obj = {
-      name: result[0].package.name,
-      des: inputvalue,
-    };
-    setselectedvalue((prev) => [...prev, obj]);
-
-    console.log(result[0].package.name);
-  }
-  function handleDelete(id) {
-    let result = selectedvalue.filter((item, index) => id !== index);
-    setselectedvalue(result);
-  }
+  const [value, setValue] = useState(null);
+  const options = [
+    { value: "Ms Dhoni", label: "Ms Dhoni" },
+    { value: "Virat", label: "Virat" },
+    { value: "Jadeja", label: "Jadeja" },
+    { value: "Ruturaj", label: "Ruturaj" },
+  ];
   return (
-    <div>
-      <input onChange={handleChange} />
-      {filtervalue &&
-        filtervalue.map((d, index) => (
-          <div>
-            <p>{d.package.name}</p>
-            <input onChange={handleChangeinput} />
-            <button onClick={() => handleClick(index)}>Add</button>
-          </div>
-        ))}
-      <h1>Add fav values</h1>
-      <table style={{ borderCollapse: "collapse", width: "100%" }}>
-        <thead>
-          <tr style={{ backgroundColor: "lightgray" }}>
-            <th style={{ padding: "8px", border: "1px solid black" }}>Name</th>
-            <th style={{ padding: "8px", border: "1px solid black" }}>
-              Description
-            </th>
-            <th style={{ padding: "8px", border: "1px solid black" }}>
-              Action
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {selectedvalue &&
-            selectedvalue.map((d, index) => (
-              <tr key={index} style={{ backgroundColor: "white" }}>
-                <td style={{ padding: "8px", border: "1px solid black" }}>
-                  {d.name}
-                </td>
-                <td style={{ padding: "8px", border: "1px solid black" }}>
-                  {d.des}
-                </td>
-                <td style={{ padding: "8px", border: "1px solid black" }}>
-                  <button
-                    style={{
-                      backgroundColor: "red",
-                      color: "white",
-                      border: "none",
-                      padding: "8px",
-                      borderRadius: "4px",
-                      cursor: "pointer",
-                    }}
-                    onClick={() => handleDelete(index)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+    <div style={{ margin: 20, width: 200 }}>
+      <Select
+        options={options}
+        defaultValue={value}
+        placeholder="Select your Player"
+        onChange={setValue}
+        isMulti
+        isSearchable
+        noOptionsMessage={() => "No Player Found"}
+        styles={{
+          clearIndicator: () => ({
+            color: "red",
+          }),
+        }}
+      />
     </div>
   );
 }
---
+++ bonusProject/src/assets/Bonus.jsx
@@ -0,0 +1,105 @@
+import { useState, useEffect } from "react";
+// import './style.css';
+
+export default function Bonus() {
+  const [data, setdata] = useState([]);
+  const [filtervalue, setfiltervalue] = useState([]);
+  const [selectedvalue, setselectedvalue] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.npms.io/v2/search?q=reactjs")
+      .then((res) => res.json())
+      .then((data) => setdata(data.results));
+  }, []);
+
+  function handleChange(e) {
+    let res = e.target.value;
+    let filtervalues = data.filter((value) => {
+      return value.package.name.toLowerCase().includes(res.toLowerCase());
+    });
+
+    if (res === "") {
+      setfiltervalue([]);
+    } else {
+      setfiltervalue(filtervalues);
+      console.log(filtervalues);
+    }
+  }
+  let inputvalue;
+  function handleChangeinput(e) {
+    inputvalue = e.target.value;
+    console.log(inputvalue);
+  }
+  function handleClick(id) {
+    let result = filtervalue.filter((value, index) => index === id);
+    let obj = {
+      name: result[0].package.name,
+      des: inputvalue,
+    };
+    setselectedvalue((prev) => [...prev, obj]);
+
+    console.log(result[0].package.name);
+  }
+  function handleDelete(id) {
+    let result = selectedvalue.filter((item, index) => id !== index);
+    setselectedvalue(result);
+  }
+  return (
+    <div>
+      <label>
+        Search Packages
+        <input onChange={handleChange} />
+      </label>
+      {filtervalue &&
+        filtervalue.map((d, index) => (
+          <div>
+            <p>{d.package.name}</p>
+            <input onChange={handleChangeinput} />
+            <button onClick={() => handleClick(index)}>Add</button>
+          </div>
+        ))}
+      <h1>Add Fav Packages</h1>
+      <table style={{ borderCollapse: "collapse", width: "100%" }}>
+        <thead>
+          <tr style={{ backgroundColor: "lightgray" }}>
+            <th style={{ padding: "8px", border: "1px solid black" }}>Name</th>
+            <th style={{ padding: "8px", border: "1px solid black" }}>
+              Description
+            </th>
+            <th style={{ padding: "8px", border: "1px solid black" }}>
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {selectedvalue &&
+            selectedvalue.map((d, index) => (
+              <tr key={index} style={{ backgroundColor: "white" }}>
+                <td style={{ padding: "8px", border: "1px solid black" }}>
+                  {d.name}
+                </td>
+                <td style={{ padding: "8px", border: "1px solid black" }}>
+                  {d.des}
+                </td>
+                <td style={{ padding: "8px", border: "1px solid black" }}>
+                  <button
+                    style={{
+                      backgroundColor: "red",
+                      color: "white",
+                      border: "none",
+                      padding: "8px",
+                      borderRadius: "4px",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => handleDelete(index)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
