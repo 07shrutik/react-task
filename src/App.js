c6abb732fca041a005baacdab6c59dec1764e102
07shrutik <shrutikmahajan1414@gmail.com>
2023-05-05 22:00:08 +0530

    calendar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
 
+export default function App() {
+  const [date, setDate] = useState(new Date());
 
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './pages/Home';
-import FavoriteList from './pages/FavoriteList';
-
-function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/favorite_list' element={<FavoriteList/>}/>
-    </Routes>
-    </BrowserRouter>
+    <div className="app" style={{ margin: "5rem", width: "60%" }}>
+      <h1 className="header">Calendar </h1>
+      <div className="calendar-container">
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <div className="text-center">Selected date: {date.toDateString()}</div>
+    </div>
   );
 }
-
-export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,23 @@
-import React from 'react'
-import './Home.css';
-import SearchInput from '../components/SearchInput'
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Home.css";
+import SearchInput from "../components/SearchInput";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className='main_div' >
-       
+    <div className="main_div">
       <div className="myapp">
-      <h1>My App</h1>
+        <h1>My App</h1>
       </div>
-      <div className='listLink'>
-            <span>
-                <Link to='/favorite_list'>Go to my favorite package list</Link>
-            </span>
-        </div>
-      <div className='searchBox'>
+      <div className="listLink">
+        <span>
+          <Link to="/favorite_list">Go to my favorite package list</Link>
+        </span>
+      </div>
+      <div className="searchBox">
         <h2>Search for NPM Packages</h2>
-        <SearchInput placeholder="Search NPM Packages"/>
+        <SearchInput placeholder="Search NPM Packages" />
       </div>
-      
     </div>
-  )
+  );
 }
