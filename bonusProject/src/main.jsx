b93d97a26b141710cb25b05eef3392cfe9276428
07shrutik <shrutikmahajan1414@gmail.com>
2023-05-08 14:10:44 +0530

    bonus project 4

--- bonusProject/src/main.jsx
+++ bonusProject/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RecoilRoot } from "recoil";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import User from "./User.jsx";
+import Score from "./Score.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/score" element={<Score />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <App /> */}
+    </RecoilRoot>
+  </React.StrictMode>
+);
